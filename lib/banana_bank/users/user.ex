203c403c3746c7bc94598a1056a246ad049fc68b
Elton Santos <eltonaxl@hotmail.com>
2023-08-15 02:55:06 -0300

    Create delete update and get

--- lib/banana_bank/users/user.ex
+++ lib/banana_bank/users/user.ex
@@ -2,9 +2,11 @@ defmodule BananaBank.Users.User do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias BananaBank.Accounts.Account
   alias Ecto.Changeset
 
-  @required_params [:name, :password, :email, :cep]
+  @required_params_create [:name, :password, :email, :cep]
+  @required_params_update [:name, :email, :cep]
 
   schema "users" do
     field(:name, :string)
@@ -12,18 +14,31 @@ defmodule BananaBank.Users.User do
     field(:password_hash, :string)
     field(:email, :string)
     field(:cep, :string)
+    has_one(:account, Account)
 
     timestamps()
   end
 
-  def changeset(user \\ %__MODULE__{}, params) do
-    user
-    |> cast(params, @required_params)
-    |> validate_required(@required_params)
-    |> validate_length(:name, min: 3)
-    |> validate_length(:cep, is: 8)
-    |> validate_format(:email, ~r/@/)
+  def changeset(params) do
+    %__MODULE__{}
+    |> cast(params, @required_params_create)
+    |> do_validations(@required_params_create)
     |> add_password_hash()
+  end
+
+  def changeset(user, params) do
+    user
+    |> cast(params, @required_params_create)
+    |> do_validations(@required_params_update)
+    |> add_password_hash()
+  end
+
+  defp do_validations(changeset, fields) do
+    changeset
+    |> validate_required(fields)
+    |> validate_length(:name, min: 3)
+    |> validate_format(:email, ~r/@/)
+    |> validate_length(:cep, is: 8)
   end
 
   defp add_password_hash(%Changeset{valid?: true, changes: %{password: password}} = changeset) do
